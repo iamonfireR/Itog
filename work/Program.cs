9343e4d23a1d78e1812910c95cb16de3dd446225
iamonfireR <89128191+iamonfireR@users.noreply.github.com>
2022-09-28 16:24:20 +0300

    321

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] baseArray = new string[6] {"item", "23", "1", "stuff", "ten", "job"};
+string[] resultArr = new string[baseArray.Length];
+
+void ArrayCheck(string[] arr, string[] arr2)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if(arr[i].Length <= 3) arr2[i] = arr[i];  
+    }
+}
+
+void Print(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+ArrayCheck(baseArray, resultArr);
+Print(resultArr);
